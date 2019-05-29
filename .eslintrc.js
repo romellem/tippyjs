1c8abfdb41fb75f1d88187441dcb9a5229e9f623
romellem <romellem.git@gmail.com>
2019-05-29 11:46:07 -0500

    Reverts repo back to v2.6.0
    This is just so I can spin up a github-pages site with the 2.6.0
    documentation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
-    jest: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  extends: 'eslint:recommended',
   parserOptions: {
-    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
   },
   rules: {
-    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/array-type': 'off',
-  },
+    indent: 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
 }
